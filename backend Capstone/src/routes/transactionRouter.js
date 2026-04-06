6462fea4ace5ffe97887719be1196e5d1bd3fc06
Gabriel Dwi Putra <gabriel.dwiputra.tangkelayuk@gmail.com>
2026-04-06 21:13:57 +0800

    adding Delete All Category Feature

--- backend Capstone/src/routes/transactionRouter.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { getTransaction, inputTransaction } from '../controller/transactionController.js'
-
-const router = express.Router()
-
-router.get('/cashflow', getTransaction) // Get All Data
-router.post('/cashflow', inputTransaction) // Create New Data
-
-export default router   
